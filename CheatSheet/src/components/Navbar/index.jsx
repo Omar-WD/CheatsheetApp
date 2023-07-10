65aad856151c9bb0d61ee91ecd6935b13dff5a02
Oldschoolove <e.g.89@hotmail.de>
2023-07-10 16:30:19 +0200

    bug fix

--- CheatSheet/src/components/Navbar/index.jsx
+++ CheatSheet/src/components/Navbar/index.jsx
@@ -7,12 +7,16 @@ const Navbar = () => {
     <div className="navContainer">
     <div className="headerItemContainer">
       <div className="leftHeaderItems">
+        <NavLink 
+        to="/"
+        style={({ isActive }) => ({marginLeft: isActive ? "5px" : "20px", marginRight: isActive ? "0px" : "15px"})}  >
         <img src="https://th.bing.com/th/id/OIP.iSzSE29bFxAhpMUwRrcPagHaHa?pid=ImgDet&rs=1" alt="logo" height={50}/>
+        </NavLink>
       </div>
 
       <NavLink 
         to="/" 
-        style={({ isActive }) => ({color: isActive ? "#fff" : "grey",})} 
+        style={({ isActive }) => ({color: isActive ? "#fff" : "grey", listStyleType: "none", textDecoration: "none", marginLeft: isActive ? "0px" : "15px"})} 
       >
         <i className="fas fa-border-all"></i>
         <span>Cheatsheets</span>
