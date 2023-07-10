24ef3d6326d058861fb2e804907bbfeeddf8c5e1
Omar-WD <129740652+Omar-WD@users.noreply.github.com>
2023-07-10 12:57:50 +0200

    adding SideBar

--- CheatSheet/src/App.jsx
+++ CheatSheet/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react"
 import './App.css'
+import SideBar from "./components/SideBar"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
+      <SideBar/>
         
     </>
   )
